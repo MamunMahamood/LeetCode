c6e2d397928bf9ebb1396db6b7425fb503ba899d
Golam Mahamood Mamun <66888954+MamunMahamood@users.noreply.github.com>
2023-09-14 09:18:16 +0600

    Added solution - LeetHub

---
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter ot = new  PrintWriter(System.out);
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.segregateElements(a, n);
+            
+            for(int i=0;i<n;i++)
+            ot.print(a[i]+" ");
+            
+            ot.println();
+        }
+        ot.close();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void segregateElements(int arr[], int n)
+    {
+        // Your code goes here
+        int[] a = new int[arr.length];
+        ArrayList<Integer> plus = new ArrayList<>();
+        ArrayList<Integer> minus = new ArrayList<>();
+        
+        
+        int m =0;
+        
+        for(int i: arr){
+            if(i>0) plus.add(i);
+            else minus.add(i);
+        }
+        
+        for(int i: plus){
+            arr[m++]=i;
+        }
+        
+        for(int i: minus){
+            arr[m++]=i;
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
